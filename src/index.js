7edddb44f8473498289c844bc3532146f9cade06
dias2020f-oss <dias2020f@gmail.com>
2026-01-05 18:44:28 -0300

    trabalho feito

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log ("oi mundo ");
+let heroi = [
+    {nome: "BleckMan", xp: 8950}
+]
+
+for (let repeticao = 0; repeticao < heroi.length; repeticao++){
+    let nomeHeroi = heroi[repeticao].nome
+    let xpHeroi = heroi[repeticao].xp
+    let nivel = ""
+    if (xpHeroi <= 1000) {
+        nivel = "ferro"
+    } else if (xpHeroi > 1001 && xpHeroi <= 2000) {
+        nivel = "bronze"
+    }else if (xpHeroi > 2001 && xpHeroi <= 5000) {
+        nivel = "prata"
+    }else if (xpHeroi > 5001 && xpHeroi <= 7000) {
+        nivel = "ouro"
+    }else if (xpHeroi > 7001 && xpHeroi <= 8000) {
+        nivel = "platina"
+    }else if (xpHeroi > 8001 && xpHeroi <= 9000) {
+        nivel = "Ascendente"
+    }else if (xpHeroi > 9001 && xpHeroi <= 10000) {
+        nivel = "Imortal"
+    }else {
+        nivel = "Radiante"
+    }
+
+        console.log("o heroi de nome " + nomeHeroi + " está no nivel " + nivel)
+
+    }
